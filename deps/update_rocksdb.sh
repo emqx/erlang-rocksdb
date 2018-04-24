7c8b83646b620ca4d348b8c6edd870b32633b269
Sergey Elin <se@exante.eu>
2018-04-24 11:36:15 +0300

    Use version instead of tag

--- deps/update_rocksdb.sh
+++ deps/update_rocksdb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-TAG=$1
-URL="https://github.com/facebook/rocksdb/archive/v$TAG.tar.gz"
+VERSION=$1
+URL="https://github.com/facebook/rocksdb/archive/v$VERSION.tar.gz"
 echo "Downloading $URL"
 curl -o /tmp/rocksdb.tar.gz -L $URL
 tar xvzf /tmp/rocksdb.tar.gz
 rm -rf rocksdb
-mv rocksdb-${TAG} rocksdb
+mv rocksdb-${VERSION} rocksdb
 echo "Use git status, add all files and commit changes."
