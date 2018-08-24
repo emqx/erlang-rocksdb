346291894f96b884287b610148f4b894205a7af6
Benoit Chesneau <bchesneau@gmail.com>
2018-08-24 18:26:16 +0200

    add simple test for the bitset merge operator

--- test/merge.erl
+++ test/merge.erl
@@ -297,3 +297,33 @@ merge_binary_insert_test() ->
 
   ok = rocksdb:close(Db),
   ok = rocksdb:destroy("/tmp/rocksdb_merge_db.test", []).
+
+
+merge_bitset_test() ->
+  [] = os:cmd("rm -rf /tmp/rocksdb_merge_db.test"),
+  {ok, Db} = rocksdb:open("/tmp/rocksdb_merge_db.test",
+                           [{create_if_missing, true},
+                            {merge_operator, {bitset_merge_operator, 1024}}]),
+
+  Bitmap = << 0:1024/unsigned >>,
+  ok = rocksdb:put(Db, <<"bitmap">>, Bitmap, []),
+  ok = rocksdb:merge(Db, <<"bitmap">>, <<"+2">>, []),
+  {ok, << 32, _/binary>> } = rocksdb:get(Db, <<"bitmap">>, []),
+  ok = rocksdb:merge(Db, <<"bitmap">>, <<"-2">>, []),
+  {ok, << 0, _/binary>> } = rocksdb:get(Db, <<"bitmap">>, []),
+  ok = rocksdb:merge(Db, <<"bitmap">>, <<"+11">>, []),
+  {ok, << 0, 16, _/binary>> } = rocksdb:get(Db, <<"bitmap">>, []),
+  ok = rocksdb:merge(Db, <<"bitmap">>, <<"+10">>, []),
+  {ok, << 0, 48, _/binary>> } = rocksdb:get(Db, <<"bitmap">>, []),
+
+  ok = rocksdb:merge(Db, <<"unsetbitmap">>, <<"+2">>, []),
+  {ok, << 32, _/binary>> } = rocksdb:get(Db, <<"unsetbitmap">>, []),
+  ok = rocksdb:merge(Db, <<"unsetbitmap">>, <<"-2">>, []),
+  {ok, << 0, _/binary>> } = rocksdb:get(Db, <<"unsetbitmap">>, []),
+  ok = rocksdb:merge(Db, <<"unsetbitmap">>, <<"+11">>, []),
+  {ok, << 0, 16, _/binary>> } = rocksdb:get(Db, <<"unsetbitmap">>, []),
+  ok = rocksdb:merge(Db, <<"unsetbitmap">>, <<"+10">>, []),
+  {ok, << 0, 48, _/binary>> } = rocksdb:get(Db, <<"unsetbitmap">>, []),
+
+  ok = rocksdb:close(Db),
+  ok = rocksdb:destroy("/tmp/rocksdb_merge_db.test", []).
