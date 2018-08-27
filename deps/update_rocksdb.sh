46b1d197be60a20cfe8d924e7f68f5df1ae0457b
Benoit Chesneau <bchesneau@gmail.com>
2018-08-27 11:41:27 +0200

    do not remove the rocksdb folder when fetch failed

--- deps/update_rocksdb.sh
+++ deps/update_rocksdb.sh
@@ -4,6 +4,9 @@ URL="https://github.com/facebook/rocksdb/archive/v$VERSION.tar.gz"
 echo "Downloading $URL"
 curl -o /tmp/rocksdb.tar.gz -L $URL
 tar xvzf /tmp/rocksdb.tar.gz
-rm -rf rocksdb
-mv rocksdb-${VERSION} rocksdb
-echo "Use git status, add all files and commit changes."
+
+if [ -d "rocksdb-${VERSION}" ]; then 
+    rm -rf rocksdb
+    mv rocksdb-${VERSION} rocksdb
+    echo "Use git status, add all files and commit changes."
+fi
