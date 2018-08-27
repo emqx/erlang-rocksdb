26559f703bca80cb24bd49d455454710d5448c5e
Benoit Chesneau <bchesneau@gmail.com>
2018-08-27 12:43:11 +0200

    add SingleDeleteCF handling to transaction log iterator
    fix #62

--- c_src/transactions.h
+++ c_src/transactions.h
@@ -91,6 +91,23 @@ public:
 
         // append to list
         t_List = enif_make_list_cell(t_Env, row, t_List);
+        return rocksdb::Status::OK();
+    }
+
+    virtual rocksdb::Status SingleDeleteCF(uint32_t column_family_id, const rocksdb::Slice& key)
+    {
+        ERL_NIF_TERM row;
+        ERL_NIF_TERM key_bin = slice_to_binary(t_Env, key);
+
+        if (column_family_id == 0) {
+            row = enif_make_tuple2(t_Env, ATOM_SINGLE_DELETE, key_bin);
+        } else {
+            ERL_NIF_TERM cf_id = enif_make_uint(t_Env, column_family_id);
+            row = enif_make_tuple3(t_Env, ATOM_SINGLE_DELETE, cf_id, key_bin);
+        }
+
+        // append to list
+        t_List = enif_make_list_cell(t_Env, row, t_List);
 
         return rocksdb::Status::OK();
     }
