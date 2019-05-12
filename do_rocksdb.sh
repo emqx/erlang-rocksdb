0bca6fea2db0e9c2d41d767390e8cf779f05585e
Ino Murko <ino.murko@outlook.com>
2019-05-12 19:49:36 +0000

    fix: set parallelism buildopts

--- do_rocksdb.sh
+++ do_rocksdb.sh
@@ -8,15 +8,17 @@ else
     CMAKE=cmake
 fi
 
-CORES=$(getconf _NPROCESSORS_ONLN 2>/dev/null || sysctl -n hw.ncpu)
-
-if [ "x$CORES" = "x" ]; then
-    PAR=""
+if [[ "$@" =~ "-j" ]]; then
+    ${CMAKE} --build . -- "$@" || exit 1
 else
-    PAR="-- -j $CORES"
+    CORES=$(getconf _NPROCESSORS_ONLN 2>/dev/null || sysctl -n hw.ncpu)
+
+    if [ "x$CORES" = "x" ]; then
+        PAR=""
+    else
+        PAR="-- -j $CORES"
+    fi
+    ${CMAKE} --build . $PAR $@ || exit 1
 fi
-
-${CMAKE} --build . "$@" $PAR || exit 1
-
 
 echo done.
