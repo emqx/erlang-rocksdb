d15ca4812b09af5cb543cb95b1a0544c961b52a9
Benoit Chesneau <bchesneau@gmail.com>
2019-11-04 17:34:13 +0100

    fix build on openbsd
    '=~' operator is not supported by sh on OpenBSD.
    
    fix #110

--- do_rocksdb.sh
+++ do_rocksdb.sh
@@ -8,17 +8,20 @@ else
     CMAKE=cmake
 fi
 
-if [[ "$@" =~ "-j" ]]; then
-    ${CMAKE} --build . -- "$@" || exit 1
-else
-    CORES=$(getconf _NPROCESSORS_ONLN 2>/dev/null || sysctl -n hw.ncpu)
+case "$@" in
+    *-j*)
+        ${CMAKE} --build . -- "$@" || exit 1
+        ;;
+    *)
+        CORES=$(getconf _NPROCESSORS_ONLN 2>/dev/null || sysctl -n hw.ncpu)
 
-    if [ "x$CORES" = "x" ]; then
-        PAR=""
-    else
-        PAR="-- -j $CORES"
-    fi
-    ${CMAKE} --build . $PAR $@ || exit 1
-fi
+        if [ "x$CORES" = "x" ]; then
+            PAR=""
+        else
+            PAR="-- -j $CORES"
+        fi
+        ${CMAKE} --build . $PAR $@ || exit 1
+        ;;
+esac
 
 echo done.
