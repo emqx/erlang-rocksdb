a4cfe6c636287944c1da772695b4eb091905f7a4
zmstone <zmstone@gmail.com>
2025-04-01 15:59:53 +0200

    build: allow cmake_minimum_required version to be older than 3.5

--- do_cmake.sh
+++ do_cmake.sh
@@ -15,6 +15,10 @@ else
     CMAKE=cmake
 fi
 
-${CMAKE} "$@" ../../c_src || exit 1
+# there are a few dependencies having cmake_minimum_required < 3.5
+# deps/lz4/contrib/cmake_unofficial/CMakeLists.txt:36:1:cmake_minimum_required (VERSION 2.8.6)
+# deps/snappy/third_party/benchmark/cmake/GoogleTest.cmake.in:1:1:cmake_minimum_required(VERSION 2.8.12)
+# deps/snappy/CMakeLists.txt:29:1:cmake_minimum_required(VERSION 3.1)
+${CMAKE} -DCMAKE_POLICY_VERSION_MINIMUM=3.5 "$@" ../../c_src || exit 1
 
 echo done-do_cmake.
