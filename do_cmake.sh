66228ad75ffe5b578c5c4177b5c7c0a0bf2a5e22
zmstone <zmstone@gmail.com>
2025-04-01 16:15:30 +0200

    build: bump cmake_minimum_required for deps

--- do_cmake.sh
+++ do_cmake.sh
@@ -15,10 +15,6 @@ else
     CMAKE=cmake
 fi
 
-# there are a few dependencies having cmake_minimum_required < 3.5
-# deps/lz4/contrib/cmake_unofficial/CMakeLists.txt:36:1:cmake_minimum_required (VERSION 2.8.6)
-# deps/snappy/third_party/benchmark/cmake/GoogleTest.cmake.in:1:1:cmake_minimum_required(VERSION 2.8.12)
-# deps/snappy/CMakeLists.txt:29:1:cmake_minimum_required(VERSION 3.1)
-${CMAKE} -DCMAKE_POLICY_VERSION_MINIMUM=3.5 "$@" ../../c_src || exit 1
+${CMAKE} "$@" ../../c_src || exit 1
 
 echo done-do_cmake.
