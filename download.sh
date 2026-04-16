feea1bf1790cf344cac979e0c7254520bed75675
zmstone <zmstone@gmail.com>
2026-04-16 10:35:33 +0200

    chore: use -sS curl option for older linux distro

--- download.sh
+++ download.sh
@@ -8,11 +8,11 @@ URL="https://github.com/emqx/erlang-rocksdb/releases/download/$TAG/$PKGNAME"
 
 mkdir -p _packages
 if [ ! -f "_packages/${PKGNAME}" ]; then
-    curl -f -L --no-progress-meter -o "_packages/${PKGNAME}" "${URL}"
+    curl -f -L -sS -o "_packages/${PKGNAME}" "${URL}"
 fi
 
 if [ ! -f "_packages/${PKGNAME}.sha256" ]; then
-    curl -f -L --no-progress-meter -o "_packages/${PKGNAME}.sha256" "${URL}.sha256"
+    curl -f -L -sS -o "_packages/${PKGNAME}.sha256" "${URL}.sha256"
 fi
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
